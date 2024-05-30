3ba4b09ae5990a15e24540d4be2f98a38a608a21
marissarh <mbutto26@gmail.com>
2024-05-29 19:22:19 -0700

    update

--- frontend/src/components/Send.jsx
+++ frontend/src/components/Send.jsx
@@ -3,7 +3,6 @@ import './Send.css'
 import { auth, db } from '../firebase'
 import { addDoc, collection, serverTimestamp} from 'firebase/firestore';
 
-<<<<<<< HEAD
 
 const Send = ({scroll}) => {
     const [input, setInput] = useState('');
@@ -27,37 +26,8 @@ const Send = ({scroll}) => {
         } catch(error){
             console.error('Error sending message:', error);
         };
-=======
-const Send = ({scroll}) => {
-    const [input, setInput] = useState(' '); 
-
-    const sendMessage = async (e) => {
-        e.preventDefault()
-        if (input.trim() === ' '){
-            alert('No message entered')
-            return
-        }
-        const { uid, displayName} = auth.currentUser;
-        try{
-            await addDoc(collection(db, 'messages'), {
-            text: input,
-            userName: displayName,
-            uid,
-            sentByCurrentUser: true,
-            timestamp: serverTimestamp()
-        });
-        setInput(' ')
-        scroll.current.scrollIntoView({behavior: 'smooth'});
-        } catch (error){
-            console.error('Error sending message:', error);
-        };
-        
-        
-        
->>>>>>> e298e4d75157e374a4620e804caa191094e9768a
     }
   return (
-<<<<<<< HEAD
     <div>
         <form className='form' onSubmit={sendMessage}>
             <input className='input'
@@ -68,16 +38,6 @@ const Send = ({scroll}) => {
             <button onClick={sendMessage}>Send</button>
         </form>
     </div>
-=======
-    <form className='form' onSubmit={sendMessage}>
-        <input className='input' 
-        type='text' 
-        placeholder='Message' 
-        value={input}
-        onChange={(e) => setInput(e.target.value)}/>
-        <button onClick={sendMessage}>Send</button>
-    </form>
->>>>>>> e298e4d75157e374a4620e804caa191094e9768a
   )
 }
 
